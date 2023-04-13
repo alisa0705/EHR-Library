1e4080064b39a156b813d090487239500e139f3a
Alisa Tian <89174034+alisa0705@users.noreply.github.com>
2023-04-13 19:55:26 -0400

    Phase5 (#13)
    * Update EHR and tests for phase5
    
    * Update README.md
    
    * Delete Phase2.py
    
    * Delete Phase1_re.py
    
    * Delete lab.txt
    
    * Delete Test_Patient.txt
    
    * Delete Patient.txt
    
    * Delete TestData_Lab.txt
    
    * Moving test files
    
    * Moving EHR
    
    * Update Patient object
    
    * Update tests
    
    * Moving fakefiles
    
    * Update test
    
    * Update test
    
    * Delete example.py
    
    * Move fake_files
    
    * Rename test.py to test_.py

---
+++ src/EHR.py
@@ -0,0 +1,129 @@
+"""Updated EHR Library."""
+from datetime import datetime
+from typing import Dict, List, Any
+
+
+class Lab:
+    """Lab class."""
+
+    def __init__(
+        self,
+        p_id: str,
+        lab_name: str,
+        lab_value: float,
+        lab_units: str,
+        lab_time: str,
+    ):
+        """Initialize the lab class."""
+        self.p_id = p_id
+        self.lab_name = lab_name
+        self.lab_value = float(lab_value)
+        self.lab_units = lab_units
+        self.lab_time = datetime.strptime(lab_time.split()[0], "%Y-%m-%d")
+
+
+class Patient:
+    """Patient class."""
+
+    def __init__(self, p_id: str, birth: str, race: str) -> None:
+        """Initialize the patient class."""
+        self.p_id = p_id
+        self.birth = datetime.strptime(birth.split()[0], "%Y-%m-%d")
+        self.race = race
+        self.labs: List[Lab] = []  # Add labs attribute to store Lab objects
+
+    def add_lab(self, lab: Lab) -> None:
+        """Add a lab to the patient's labs."""
+        if lab.p_id == self.p_id:
+            self.labs.append(lab)
+
+    @property
+    def age(self) -> int:
+        """Return the age of the patient."""
+        today = datetime.today()
+        if self.birth > today:
+            raise ValueError("Invalid birth date")
+        age_years = (
+            today.year
+            - self.birth.year
+            - ((today.month, today.day) < (self.birth.month, self.birth.day))
+        )
+        return age_years
+
+    def is_sick(self, lab_name: str, operator: str, value: float) -> bool:
+        """Return a boolean indicating whether sick or not."""
+        if operator not in [">", "<", "="]:
+            raise ValueError("Invalid comparison operator")
+
+        for lab in self.labs:
+            if lab.p_id == self.p_id and lab.lab_name == lab_name:
+                lab_value = lab.lab_value
+                if (
+                    (operator == ">" and lab_value > value)
+                    or (operator == "<" and lab_value < value)
+                    or (operator == "=" and lab_value == value)
+                ):
+                    return True
+        return False
+
+    def age_since_earliest_lab(self) -> int:
+        """Return the age of the patient at the time of their first lab."""
+        patient_labs = [lab for lab in self.labs if lab.p_id == self.p_id]
+        if not patient_labs:
+            raise ValueError(f"No lab records found for {self.p_id}")
+
+        fst_l = min(patient_labs, key=lambda lab: lab.lab_time).lab_time
+        age_years = (
+            fst_l.year
+            - self.birth.year
+            - ((fst_l.month, fst_l.day) < (self.birth.month, self.birth.day))
+        )
+
+        return age_years
+
+
+def parse_file(filename: str) -> Dict[str, List[str]]:
+    """Parse the file."""
+    with open(filename, encoding="UTF-8-sig") as file:
+        lines = file.readlines()
+    if not lines:
+        return {}
+
+    header = lines[0].strip().split("\t")
+    data_dict: Dict[str, List[str]] = {col: [] for col in header}
+
+    for line in lines[1:]:
+        values = line.strip().split("\t")
+        for i, value in enumerate(values):
+            data_dict[header[i]].append(value)
+    return data_dict
+
+
+def parse_data(
+    patient_filename: str, lab_filename: str
+) -> tuple[List[Patient], List[Lab]]:
+    """Read and parse the patient and lab data files."""
+    patient_data = parse_file(patient_filename)
+    lab_data = parse_file(lab_filename)
+
+    patients = [
+        Patient(p_id, dob, race)
+        for p_id, dob, race in zip(
+            patient_data["PatientID"],
+            patient_data["PatientDateOfBirth"],
+            patient_data["PatientRace"],
+        )
+    ]
+
+    labs = [
+        Lab(p_id, lab_name, float(lab_value), lab_units, lab_time)
+        for p_id, lab_name, lab_value, lab_units, lab_time in zip(
+            lab_data["PatientID"],
+            lab_data["LabName"],
+            lab_data["LabValue"],
+            lab_data["LabUnits"],
+            lab_data["LabDateTime"],
+        )
+    ]
+
+    return patients, labs
---
+++ tests/test_.py
@@ -0,0 +1,214 @@
+"""Testing for EHR."""
+from typing import Dict, List
+from EHR import parse_file, parse_data, Patient, Lab
+from fake_files import fake_files
+import pytest
+
+
+def test_parse_file() -> None:
+    """Test parse_data() function."""
+    test_data = [
+        [
+            "PatientID",
+            "PatientDateOfBirth",
+            "PatientRace",
+        ],
+        ["MB2A", "1960-01-01", "White"],
+        ["A418", "1970-07-25", "Asian"],
+        ["CB22", "1980-08-30", "Black"],
+        ["HJF0", "1990-09-01", "Hispanic"],
+        ["CFEAA0", "2000-10-03", "White"],
+    ]
+
+    with fake_files(test_data) as temp_files:
+        data: Dict[str, List[str]] = parse_file(temp_files[0])
+        assert data["PatientID"] == [
+            "MB2A",
+            "A418",
+            "CB22",
+            "HJF0",
+            "CFEAA0",
+        ]
+        assert len(data["PatientDateOfBirth"]) == 5
+
+
+def test_parse_data() -> None:
+    """Test parse_data() function."""
+    patient_data_list = [
+        ["PatientID", "PatientDateOfBirth", "PatientRace"],
+        ["MB2A", "1960-01-01", "White"],
+        ["A418", "1970-07-25", "Asian"],
+        ["CB22", "1980-08-30", "Black"],
+        ["HJF0", "1990-09-01", "Hispanic"],
+        ["CFEAA0", "2000-10-03", "White"],
+    ]
+
+    lab_data_list = [
+        ["PatientID", "LabName", "LabValue", "LabUnits", "LabDateTime"],
+        ["MB2A", "CBC: WBC", "10.3", "K/uL", "1990-06-01 01:02:03.456"],
+        ["A418", "CBC: Hct", "45.2", "%", "1991-07-01 01:20:34.567"],
+        ["CB22", "CBC: MCH", "32.1", "pg", "1992-08-01 01:23:45.678"],
+        ["HJF0", "CBC: MCV", "98.5", "fL", "1993-09-01 01:24:56.789"],
+        ["CFEAA0", "CBC: RDW", "12.8", "%", "1994-10-01 01:25:67.890"],
+    ]
+
+    with fake_files(patient_data_list, lab_data_list) as temp_files:
+        patient_filename, lab_filename = temp_files
+        patient_data, lab_data = parse_data(patient_filename, lab_filename)
+
+        # Test patient data
+        assert isinstance(patient_data, list)
+        assert len(patient_data) == 5
+
+        # Test lab data
+        assert isinstance(lab_data, list)
+        assert len(lab_data) == 5
+        assert lab_data[0].p_id == "MB2A"
+
+
+def test_age_since_earliest_lab() -> None:
+    """Test age_since_earliest_lab() function."""
+    patient_data_list = [
+        ["PatientID", "PatientDateOfBirth", "PatientRace"],
+        ["MB2A", "1960-01-01", "White"],
+        ["A418", "1970-07-25", "Asian"],
+        ["CFEAA0", "2000-10-03", "White"],
+    ]
+
+    lab_data_list = [
+        ["PatientID", "LabName", "LabValue", "LabUnits", "LabDateTime"],
+        ["MB2A", "CBC: WBC", "10.3", "K/uL", "1990-06-01 01:02:03.456"],
+        ["A418", "CBC: Hct", "45.2", "%", "1991-07-01 01:20:34.567"],
+        ["CB22", "CBC: MCH", "32.1", "pg", "1992-08-01 01:23:45.678"],
+        ["HJF0", "CBC: MCV", "98.5", "fL", "1993-09-01 01:24:56.789"],
+        ["CFEAA0", "CBC: RDW", "12.8", "%", "1994-10-01 01:25:67.890"],
+    ]
+    with fake_files(patient_data_list, lab_data_list) as temp_files:
+        patient_filename, lab_filename = temp_files
+        patients, labs = parse_data(patient_filename, lab_filename)
+        patient = next((p for p in patients if p.p_id == "MB2A"), None)
+        assert patient is not None
+
+        # Assign labs to the corresponding patients
+        for lab in labs:
+            patient = next((p for p in patients if p.p_id == lab.p_id), None)
+            if patient:
+                patient.add_lab(lab)
+
+        patient = next((p for p in patients if p.p_id == "MB2A"), None)
+        assert patient is not None
+        # Test with valid patient ID
+        age = patient.age_since_earliest_lab()
+        assert age == 30
+
+
+def test_age_since_earliest_lab_value_error() -> None:
+    """Test age_since_earliest_lab() function with invalid patient ID."""
+    patient_data_list = [
+        ["PatientID", "PatientDateOfBirth", "PatientRace"],
+        ["MB2A", "1960-01-01", "White"],
+        ["A418", "1970-07-25", "Asian"],
+        ["CFEAA0", "2000-10-03", "White"],
+    ]
+
+    lab_data_list = [
+        ["PatientID", "LabName", "LabValue", "LabUnits", "LabDateTime"],
+        ["MB2A", "CBC: WBC", "10.3", "K/uL", "1990-06-01 01:02:03.456"],
+        ["A418", "CBC: Hct", "45.2", "%", "1991-07-01 01:20:34.567"],
+        ["CB22", "CBC: MCH", "32.1", "pg", "1992-08-01 01:23:45.678"],
+        ["HJF0", "CBC: MCV", "98.5", "fL", "1993-09-01 01:24:56.789"],
+        ["CFEAA0", "CBC: RDW", "12.8", "%", "1994-10-01 01:25:67.890"],
+    ]
+    with fake_files(patient_data_list, lab_data_list) as temp_files:
+        patient_filename, lab_filename = temp_files
+        patients, labs = parse_data(patient_filename, lab_filename)
+
+        # Assign labs to the corresponding patients
+        for lab in labs:
+            patient = next((p for p in patients if p.p_id == lab.p_id), None)
+            if patient:
+                patient.add_lab(lab)
+
+        # Test with invalid patient ID
+        with pytest.raises(ValueError, match="No lab records found for x"):
+            invalid_patient = Patient("x", "2000-01-01", "Unknown")
+            age = invalid_patient.age_since_earliest_lab()
+
+
+def test_patient_age() -> None:
+    """Test patient_age() function."""
+    patient_data_test = {
+        "PatientID": ["MB2A", "A418", "CB22", "HJF0", "CFEAA0"],
+        "PatientDateOfBirth": [
+            "1960-01-01",
+            "1970-07-25",
+            "1980-08-30",
+            "1990-09-01",
+            "2000-10-03",
+        ],
+    }
+    patient = Patient("MB2A", "1960-01-01", "White")
+    assert patient.age == 63
+
+
+def test_patient_age_value_error() -> None:
+    """Test ValueError for patient_age() function."""
+    patient = Patient("abcd", "2100-01-01", "Unknown")
+
+    with pytest.raises(ValueError):
+        age = patient.age
+
+
+def test_patient_is_sick_greater_than() -> None:
+    """Test patient_is_sick() function with the '>' operator."""
+    # Create Patient and Lab instances
+    patient = Patient("MB2A", "1960-01-01", "White")
+    lab = Lab("MB2A", "GLUCOSE", 150, "mg/dL", "1990-06-01")
+
+    # Add the lab to the patient's labs
+    patient.add_lab(lab)
+
+    # Test '>' operator
+    assert isinstance(patient.is_sick("GLUCOSE", ">", 100), bool)
+    assert patient.is_sick("GLUCOSE", ">", 100) is True
+
+
+def test_patient_is_sick_less_than() -> None:
+    """Test patient_is_sick() function with the '<' operator."""
+    # Create Patient and Lab instances
+    patient = Patient("MB2A", "1960-01-01", "White")
+    lab = Lab("MB2A", "GLUCOSE", 150, "mg/dL", "1990-06-01")
+
+    patient.add_lab(lab)
+
+    # Test '<' operator
+    assert not patient.is_sick("GLUCOSE", "<", 100)
+
+
+def test_patient_is_sick_equal_to() -> None:
+    """Test patient_is_sick() function with the '=' operator."""
+    # Create Patient and Lab instances
+    patient = Patient("A418", "1970-07-25", "Asian")
+    lab = Lab("A418", "METABOLIC: CALCIUM", 8.9, "mg/dL", "1991-07-01")
+    patient.add_lab(lab)
+
+    # Test '=' operator
+    assert patient.is_sick("METABOLIC: CALCIUM", "=", 8.9) is True
+
+
+def test_patient_is_sick_value_error() -> None:
+    """Test ValueError for patient_is_sick function."""
+    patient1 = Patient("MB2A", "1960-01-01", "White")
+
+    lab1 = Lab("MB2A", "GLUCOSE", 150, "mg/dL", "1990-06-01")
+    lab2 = Lab("A418", "METABOLIC: CALCIUM", 8.9, "mg/dL", "1991-07-01")
+
+    # Add labs to the patient's labs
+    patient1.add_lab(lab1)
+
+    with pytest.raises(ValueError):
+        patient1.is_sick("METABOLIC: ALK PHOS", "WRONG", 60)
+
+
+if __name__ == "__main__":
+    pytest.main(["-v", "test.py"])
